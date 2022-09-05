9b4d005943080940b0e6241496c6e32d99d9494e
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-05 17:19:45 -0300

    Elemento CSS - Component

---
+++ Lista.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Lista = styled.li`
+  background-color: #ac5e20;
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  font-family: Arial, sans-serif;
+`;
+
+export default Lista;
