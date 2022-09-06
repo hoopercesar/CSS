4b172087ddbd6d54948ab590dbd5230d1cf8e8f5
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-06 08:20:47 -0300

    styled-components de css
    características dinámicas de un botón, programadas usando styled components en JS.
    Es posible agregar nuevas características creando funciones que cambien propiedades específicas como color
    ancho, márgenes, tamaños, etc, y aplicarla una o más de estas características a cada botón del APP.

--- Boton.js
+++ Boton.js
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
+// estas son las características del boton
 const Boton = styled.button`
   background: #83d394;
   display: inline-block;
@@ -10,6 +11,32 @@ const Boton = styled.button`
   font-family: Arial, sans-serif;
   cursor: pointer;
   transition: 0.3s ease all;
+  width: ${(props) => (props.largo ? "100%" : "auto")};
+
+  &:hover {
+    background: #44a559;
+    color: #fff;
+  }
+
+  /*estas líneas cambian las propiedades de cualquier objeto css
+  a la que le ponga negro. (ver botones incremento/decremento)*/
+
+  ${(props) =>
+    props.negro &&
+    css`
+      background: #000;
+      color: #fff;
+    `}
+  ${(props) =>
+    props.margenSuperior &&
+    css`
+      margin-top: 10px;
+    `}  
+  ${(props) =>
+    props.margenDerecho &&
+    css`
+      margin-right: 10px;
+    `}
 `;
 
 export default Boton;
